8f04737f844bf801233c723c111bd85c42954f63
rayan12342003 <rayanlepaz@gmail.com>
2024-12-21 03:06:01 +0100

    Initial commit with chatbot project

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,22 @@
-FROM python:3.10-slim
+
+FROM openjdk:11-jdk-slim
+
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+
+
+RUN apt-get update && apt-get install -y python3 python3-pip
+
+
 WORKDIR /app
 
-COPY . /app
-
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
+
 COPY . .
+
+
 EXPOSE 5000
+
+
 CMD ["flask", "run", "--host=0.0.0.0", "--port=5000"]
--- scripts/refomul.py
+++ scripts/refomul.py
@@ -1,15 +1,28 @@
 from spellchecker import SpellChecker
 from textblob import TextBlob
+import language_tool_python
+
 
 spell = SpellChecker()
+tool = language_tool_python.LanguageTool('en-US')
+
 
 def correct_spelling(input_text):
+   
     input_text = input_text.lower().strip()
+
+
     words = input_text.split()
     misspelled = spell.unknown(words)
     corrected_words = [spell.correction(word) if word in misspelled else word for word in words]
     corrected_text = " ".join(corrected_words)
+
+
     corrected_text = str(TextBlob(corrected_text).correct())
 
-    return corrected_text
+    matches = tool.check(corrected_text)
+    corrected_text = language_tool_python.utils.correct(corrected_text, matches)
 
+   
+
+    return corrected_text
